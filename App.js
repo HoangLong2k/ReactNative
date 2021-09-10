d07c504e5922c51184e6f8e49c9bf40984428973
longhoang <81526758+longle2concho@users.noreply.github.com>
2021-09-10 13:36:44 +0700

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
+      <Text>My name is Long</Text>     
+      <Text>This is my first React Native App</Text>     
+      <Text>I Love you</Text>     
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:"10px"
   },
 });
